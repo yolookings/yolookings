07e1f80f09a33f1f8533e3c93b14db767bd45613
Maulana Zahiri <147925317+yolookings@users.noreply.github.com>
2024-05-29 00:09:46 +0700

    Delete intro.c

--- intro.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Menampilkan perkenalan
-    printf("Nama: Maulana Ahmad Zahiri\n");
-    printf("Keterangan: Saya seorang pengembang perangkat lunak yang tertarik pada bidang kecerdasan buatan dan pemrograman web.\n");
-    printf("maulanazahiri31@gmail.com\n");
-    return 0;
-}
